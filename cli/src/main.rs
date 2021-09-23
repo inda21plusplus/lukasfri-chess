b0c1405b1962d152d3832403b54a23c1f8895c1d
Lukas Friman <lukas@dreamplay.net>
2021-09-23 19:23:40 +0200

    Fix print_board function.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,24 +1,25 @@
 use std::str::FromStr;
 
 use colored::{ColoredString, Colorize};
-use my_library::{game::Game, structs::Color};
+use my_library::{game::Game, structs::{Color, Coordinate}};
 
 fn print_board(game: &Game) {
-    for (x_coord, x) in (&game.board.0).iter().enumerate() {
-        let mut output_string = String::new();
-        for (y_coord, y) in (x).iter().enumerate() {
+    let mut output_string = String::new();
+    for y_coord in (0..game.board.height).rev() {
+        output_string.push_str(format!("{} ", y_coord + 1).as_str());
+        for (x_coord, square) in game.board.pieces[y_coord*game.board.width..(y_coord+1)*game.board.width].iter().enumerate() {
             let mut piece_color: Option<Color> = None;
             let mut string = String::new();
-            if !y.0 || y.1.is_none() {
+            if square.is_empty() {
                 string.push(' ');
             } else {
-                let piece = y.1.as_ref().unwrap();
+                let piece = square.unwrap();
                 string.push_str(piece.get_char());
                 piece_color = Some(piece.get_color());
             }
             string.push(' ');
 
-            let mut colored_string: ColoredString = if ((y_coord + (x_coord % 2)) % 2) == 0 {
+            let mut colored_string: ColoredString = if (x_coord + (y_coord % 2)) % 2 == 0 {
                 string.on_black()
             } else {
                 string.on_bright_black()
@@ -37,13 +38,23 @@ fn print_board(game: &Game) {
             };
 
             output_string.push_str(format!("{}", colored_string).as_str());
-        }
 
-        print!("{}\n", output_string);
+            if x_coord + 1 == game.board.width { output_string.push_str("\n"); };
+        }
     }
+    output_string.push_str("  ");
+    for i in 1..=game.board.width {
+        output_string.push_str(format!("{} ", i).as_str());
+    }
+
+    print!("{}\n", output_string);
 }
 
 fn main() {
-    let game: Game = Game::new();
+    let mut game: Game = Game::new();
+    print_board(&game);
+    game.move_piece(&Coordinate{x: 0, y: 0}, &Coordinate{x: 0, y: 5});
+    print_board(&game);
+    game.move_piece(&Coordinate{x: 0, y: 0}, &Coordinate{x: 0, y: 1});
     print_board(&game);
 }
